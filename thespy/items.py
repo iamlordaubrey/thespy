580b9db9dfd51062336ccfdf356df0d180d93a5c
Chiemeka Alim <chiemeka.alim@andela.co>
2016-12-05 21:52:36 +0100

    Define fields for movie item; Create class OzSpy

--- thespy/items.py
+++ thespy/items.py
@@ -11,4 +11,8 @@ import scrapy
 class ThespyItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    title = scrapy.Field()
+    starring = scrapy.Field()
+    show_times = scrapy.Field()
--- thespy/spiders/thewizard.py
+++ thespy/spiders/thewizard.py
@@ -43,4 +43,80 @@ def format_synopsis(synopsis_list):
 
 
 class OzSpy(Spider):
-    pass
+    name = 'ozonecinemas'
+    allowed_domains = ['ozonecinemas.com',]
+    start_urls = ['http://ozonecinemas.com/now_showing.htm',]
+
+    def parse(self, response):
+        raw_data = response.xpath("//td[@id='content-area']/table//table//td")
+
+        # Extract and assign relevant data to the movies list
+        movies_tuples = zip(raw_data[2:-2:3], raw_data[3:-2:3])
+        movies = list(chain(*movies_tuples))
+        base_url = 'http://ozonecinemas.com/'
+
+        for movie in movies:
+            item = ThespyItem()
+
+            # Movie Image
+            rel = movie.xpath('img/@src').extract()
+            if rel:
+                image_url = [base_url + ' '.join(str(rel[0]).split())]
+                item['image_urls'] = image_url
+
+            # Movie Title
+            raw_title = movie.xpath('p/strong/text()').extract()
+            if raw_title:
+                m_title = ' '.join(str(raw_title[0]).split())
+                item['title'] = m_title
+
+            # Movie Starring
+            l_starring = movie.xpath('p/strong/text()').extract()
+            print('l_starring', l_starring, 'l_starring')
+            r_starring = movie.xpath('p/strong[2]/following-sibling::text()[1]').extract()
+
+            if l_starring and r_starring:
+                l = [x.strip() for x in r_starring[0].split(',')]
+
+                actors = []
+                for j in l:
+                    j = ''.join([k if ord(k) < 128 else '' for k in j])
+                    actors.append(' '.join(str(j).split()))
+
+                actors = ', '.join(actors)
+                item['starring'] = actors
+
+            # Movie Show times
+            show_times = movie.xpath("p[text()][contains(., 'Mon:') or "
+                                     "contains(., 'Tue:') or"
+                                     "contains(., 'Wed:') or "
+                                     "contains(., 'Thur:') or "
+                                     "contains(., 'Fri:') or "
+                                     "contains(., 'Sat:') or "
+                                     "contains(., 'Sun:')]").extract()
+
+            if show_times:
+                lst_times = []
+                for i in show_times:
+                    lst_times.append(
+                        ' '.join(str(i).replace('&amp;', '&').strip('</p>').translate(
+                            None, '\t\n'
+                        ).split())
+                    )
+
+                item['show_times'] = ''.join(lst_times)
+
+            # Movie Synopsis
+            movie_synopsis = []
+
+            # Loop through paragraphs 6-10 searching for movie synopsis
+            for i in range(6, 10):
+                formatted_synopsis = extract_synopsis(movie, i)
+                if formatted_synopsis:
+                    movie_synopsis.append(formatted_synopsis)
+                    break
+
+            if movie_synopsis:
+                item['synopsis'] = ''.join(movie_synopsis)
+
+            yield item
